9cc76f1739464e39adc30075cf220b6b538451ed
GPV <104330277+GPVcode@users.noreply.github.com>
2022-06-05 13:55:44 -0700

    Update BMI Calculator.js

--- BMI Calculator.js
+++ BMI Calculator.js
@@ -1,26 +1,15 @@
-/*This is my original version without the prompt
-//setting function myBMI using weight and height input
-function myBMI(weight, height){
-    //Set rounded answer of the BMI formula and store this value in variable called BMI
-    var BMI = Math.round(weight/(height*height));
-    //printingconcatenated for developer observing.
-    console.log("Based on your height, your BMI is " + BMI + ".");
+
+// bmi CALCULATOR WITH USER FEEDBACK
+//Set up function called bmiCalculator using inputs weight and height
+function bmiCalculator (weight, height) {
+    var bmi = weight/(height*height); // this is the formula for BMI stored inside variable called bmi
+    if (bmi < 18.5){ //if else conditions to give user feedback based on the BMI calculated from their inputs
+        return("Your BMI is " + bmi + ", so you are underweight.")
+    } else if (bmi >= 18.5 && bmi <= 24.9){
+        return("Your BMI is " + bmi + ", so you have a normal weight.")
+    } else{
+        return("Your BMI is " + bmi + ", so you are overweight.")
+    }
 }
 
-//Calling on weight(kg) and height(cm) of function myBMI
-myBMI(300, 60);
-
-*/
-
-
-
-//The follow code is my work in response to a prompt
-//The first parameter should be the weight and the second should be the height.
-function bmiCalculator(weight, height){
-    // Storing the function in a variable named bmi
-    var bmi = bmiCalculator(weight, height);
-    //returning the calculation of BMI and rounding the answer.
-    return Math.round(weight/(height*height));
-    //another way to write this is: return Math.round(weight/Math.pow(height,2);
-}
-
+//NOTES: we can, but aren't using alert, prompt, or console.
